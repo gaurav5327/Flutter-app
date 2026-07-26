b841ba07934369bc5fbe9cf9f4f0922837d22ed2
Gaurav Salunke <gauravsalunke252@gmail.com>
2026-07-26 18:55:45 +0530

    Add plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yttutorial/pages/homepage.dart';
+import 'package:yttutorial/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Material(
-      child:Center(
-        child: Container(
-          child:Text("Welcome to flutter"),
-        ),
-      )
-      )
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily:GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => Loginpage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => Loginpage(),
+      },
     );
   }
 }
